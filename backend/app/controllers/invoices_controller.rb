6d140ec09d1f2c50b6588c87627b01115bdb6a5a
Nate Cornell <nate.cornell@xenplatforms.com>
2023-07-05 15:05:04 -0700

    Add routes for new invoice controller actions. Add error responses from controller

--- backend/app/controllers/invoices_controller.rb
+++ backend/app/controllers/invoices_controller.rb
@@ -14,20 +14,28 @@ class InvoicesController < ApplicationController
 
   def create
     @invoice = Invoice.new(invoice_params)
+
     if @invoice.save
       render json: @invoice
     else
-      render json: { error: 'Error creating invoice' }
+      render json: { error: 'Error creating invoice' }, status: :bad_request
     end
   end
 
   def update
     @invoice = Invoice.find(params[:id])
-    if @invoice
-      @invoice.update(invoice_params)
-      render json: { message: 'Invoice successfully updated' }
+
+    if !@invoice
+      render json: { error: "Could not find invoice id #{params[:id]}" }, status: :not_found
+      return
+    end
+
+    @invoice.update(invoice_params)
+
+    if @invoice.errors.any?
+      render json: { error: 'Error updating invoice', errors: @invoice.errors }, status: :bad_request
     else
-      render json: { error: 'Error updating invoice' }
+      render json: { message: 'Invoice successfully updated', invoice: @invoice }
     end
   end
 
@@ -35,33 +43,71 @@ class InvoicesController < ApplicationController
     params.require(:invoice).permit(:invoice_number, :due_date, :amount, :state)
   end
 
+  def complete
+    @invoice = Invoice.find(params[:id])
+
+    if !@invoice
+      render json: { error: "Could not find invoice id #{params[:id]}" }, status: :not_found
+      return
+    end
+
+    @invoice.complete!
+
+    if @invoice.errors.any?
+      render json: { error: 'Error completing invoice', errors: @invoice.errors }, status: :bad_request
+    else
+      render json: { message: 'Invoice successfully completed', invoice: @invoice }
+    end
+  end
+
   def pay
     @invoice = Invoice.find(params[:id])
-    if @invoice
-      @invoice.pay!
-      render json: { message: 'Invoice successfully payed' }
+
+    if !@invoice
+      render json: { error: "Could not find invoice id #{params[:id]}" }, status: :not_found
+      return
+    end
+
+    @invoice.pay!
+
+    if @invoice.errors.any?
+      render json: { error: 'Error paying invoice', errors: @invoice.errors }, status: :bad_request
     else
-      render json: { error: 'Error paying invoice' }
+      render json: { message: 'Invoice successfully paid', invoice: @invoice }
     end
   end
 
   def ship
     @invoice = Invoice.find(params[:id])
-    if @invoice
-      @invoice.ship!
-      render json: { message: 'Invoice successfully shipped' }
+
+    if !@invoice
+      render json: { error: "Could not find invoice id #{params[:id]}" }, status: :not_found
+      return
+    end
+
+    @invoice.ship!
+
+    if @invoice.errors.any?
+      render json: { error: 'Error shipping invoice', errors: @invoice.errors }, status: :bad_request
     else
-      render json: { error: 'Error shipping invoice' }
+      render json: { message: 'Invoice successfully shipped', invoice: @invoice }
     end
   end
 
   def void
     @invoice = Invoice.find(params[:id])
-    if @invoice
-      @invoice.void!
-      render json: { message: 'Invoice successfully voided' }
+
+    if !@invoice
+      render json: { error: "Could not find invoice id #{params[:id]}" }, status: :not_found
+      return
+    end
+
+    @invoice.void!
+
+    if @invoice.errors.any?
+      render json: { error: 'Error voiding invoice', errors: @invoice.errors }, status: :bad_request
     else
-      render json: { error: 'Error voiding invoice' }
+      render json: { message: 'Invoice successfully voided', invoice: @invoice }
     end
   end
 end

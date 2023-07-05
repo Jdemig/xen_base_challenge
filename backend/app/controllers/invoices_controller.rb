4e990b0ec61a766ecbce0aa64115c525e1067ec9
Nate Cornell <nate.cornell@xenplatforms.com>
2023-07-05 11:40:58 -0700

    Add state field and method actions to invoice. Add controller actions for managing state

--- backend/app/controllers/invoices_controller.rb
+++ backend/app/controllers/invoices_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Invoices Controller
 class InvoicesController < ApplicationController
   def index
     @invoices = Invoice.all
@@ -14,7 +17,7 @@ class InvoicesController < ApplicationController
     if @invoice.save
       render json: @invoice
     else
-      render json: {error: 'Error creating invoice'}
+      render json: { error: 'Error creating invoice' }
     end
   end
 
@@ -22,13 +25,43 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     if @invoice
       @invoice.update(invoice_params)
-      render json: {message: 'Invoice successfully updated'}
+      render json: { message: 'Invoice successfully updated' }
     else
-      render json: {error: 'Error updating invoice'}
+      render json: { error: 'Error updating invoice' }
     end
   end
 
   def invoice_params
-    params.require(:invoice).permit(:invoice_number, :due_date, :amount)
+    params.require(:invoice).permit(:invoice_number, :due_date, :amount, :state)
+  end
+
+  def pay
+    @invoice = Invoice.find(params[:id])
+    if @invoice
+      @invoice.pay!
+      render json: { message: 'Invoice successfully payed' }
+    else
+      render json: { error: 'Error paying invoice' }
+    end
+  end
+
+  def ship
+    @invoice = Invoice.find(params[:id])
+    if @invoice
+      @invoice.ship!
+      render json: { message: 'Invoice successfully shipped' }
+    else
+      render json: { error: 'Error shipping invoice' }
+    end
+  end
+
+  def void
+    @invoice = Invoice.find(params[:id])
+    if @invoice
+      @invoice.void!
+      render json: { message: 'Invoice successfully voided' }
+    else
+      render json: { error: 'Error voiding invoice' }
+    end
   end
 end
